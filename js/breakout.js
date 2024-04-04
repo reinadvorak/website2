5b77af0ccd6f8cdc731722340f3c0162d743d5ec
reinadvorak <142573724+reinadvorak@users.noreply.github.com>
2024-04-04 06:15:10 +0000

    Wed, Apr 3, 2024, 11:15 PM -07:00

--- js/breakout.js
+++ js/breakout.js
@@ -168,6 +168,20 @@ function moveBall() {
     ) {
         ball.dy = -1 * ball.dy
     }
+
+    //Brick collision
+    bricks.forEach(column => {
+        column.forEach(brick => {
+            if (brick.visible) {
+                if (
+                    ball.y - ball.size < brick.y + brick.h //bottom
+                ) {
+                ball.dy = -1 * ball.dy
+                brick.visible.false
+                }
+            }
+        })
+    })
 }
 
 // Update canvas drawing and animation
