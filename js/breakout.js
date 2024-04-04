e771afe79cffe8a839d6ad54159edae811af32b1
reinadvorak <142573724+reinadvorak@users.noreply.github.com>
2024-04-04 06:23:40 +0000

    Wed, Apr 3, 2024, 11:23 PM -07:00

--- js/breakout.js
+++ js/breakout.js
@@ -169,7 +169,7 @@ function moveBall() {
         ball.x + ball.size < paddle.x + paddle.w &&
         ball.y + ball.size > paddle.y
     ) {
-        ball.dy = -1 * ball.dy
+        ball.dy = -1 * ball.speed
     }
 
     //Brick collision
