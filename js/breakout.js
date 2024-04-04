84b78626758ee683c81f001fe9275d4ffe80e79d
reinadvorak <142573724+reinadvorak@users.noreply.github.com>
2024-04-04 06:11:15 +0000

    Wed, Apr 3, 2024, 11:11 PM -07:00

--- js/breakout.js
+++ js/breakout.js
@@ -164,7 +164,7 @@ function moveBall() {
     if (
         ball.x - ball.size > paddle.x &&
         ball.x + ball.size < paddle.x + paddle.w &&
-        ball.y + ball.size < paddle.y
+        ball.y + ball.size > paddle.y
     ) {
         ball.dy = -1 * ball.dy
     }
