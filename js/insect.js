9384452a951e657e3cbf81323414a5f538b5ce9c
reinadvorak <142573724+reinadvorak@users.noreply.github.com>
2024-04-10 05:18:03 +0000

    Tue, Apr 9, 2024, 10:18 PM -07:00

--- js/insect.js
+++ js/insect.js
@@ -31,7 +31,7 @@ function createInsect() {
     const { x, y } = getRandomLocation()
     insect.style.top = `${y}px`
     insect.style.left = `${x}px`
-    insect.innerHTML = `<img src="${selected_insect.src}" alt="${selected_insects.alt}" style = "transform: rotate(${Math.random() * 360}deg" />`
+    insect.innerHTML = `<img src="${selected_insect.src}" alt="${selected_insects.alt}" style = "transform: rotate(${Math.random() * 360}deg") />`
 
     game_container.appendChild(insect)
 }
