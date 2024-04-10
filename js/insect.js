353a2e0e9b2d4044eb965e892133d76c036357a1
reinadvorak <142573724+reinadvorak@users.noreply.github.com>
2024-04-10 04:40:03 +0000

    Tue, Apr 9, 2024, 9:40 PM -07:00

--- js/insect.js
+++ js/insect.js
@@ -1,4 +1,11 @@
 const screens = document.querySelectorAll('.screen')
 const choose_insect_btns = querySelectorAll('.choose-insect-btn')
-const
+const game_container = document.getElementById('game-container')
+const start_btn = document.getElementById('start-btn')
+const timeEl = document.getElementById('time')
+const scoreEl = document.getElementById('score')
+const message = document.getElementById('message')
+let seconds = 0
+let score = 0
+let selected-insect = {}
 
