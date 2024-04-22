89587c0ca5751f664ee9894eca37b7ec72a4f6ca
reinadvorak <142573724+reinadvorak@users.noreply.github.com>
2024-04-22 22:07:08 +0000

    Mon, Apr 22, 2024, 3:07 PM -07:00

--- js/hangman.js
+++ js/hangman.js
@@ -39,6 +39,7 @@ window.addEventListener('keydown', e => {
     if (e.keyCode >= 65 && e.keyCode <= 90) {
         const letter = e.key
 
+
         if(selectedWord.includes(letter)) {
             if(!correctLetters.includes(letter)) {
                 correctLetters.push(letter)
@@ -50,6 +51,10 @@ window.addEventListener('keydown', e => {
         } else {
             if (!wrongLetters.includes(letter)) {
                 wrongLetters.push(letter)
+
+                updateWrongLettersEl()
+            } else {
+                showNotification()
             }
         }
     }
