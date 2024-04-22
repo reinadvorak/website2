3086b5e5c1b26f22a28ab60174c2e7920c048750
reinadvorak <142573724+reinadvorak@users.noreply.github.com>
2024-04-22 21:53:39 +0000

    Mon, Apr 22, 2024, 2:53 PM -07:00

--- js/hangman.js
+++ js/hangman.js
@@ -27,7 +27,11 @@ function displayWord() {
     }
     `
     const innerWord = wordEl.innerText.replace(/\n/g, '')
-    console.log(wordEl.innerText, innerWord)
+
+    if (innerWord == selectedWord) {
+        finalMessage.innerText = 'Congratulations! You won!'
+        popup.style.display = 'flex'
+    }
 }
 
 displayWord()
