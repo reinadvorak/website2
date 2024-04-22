1441edb04e3e4ca330a313c5548a04f8943f16b2
reinadvorak <142573724+reinadvorak@users.noreply.github.com>
2024-04-22 21:31:19 +0000

    Mon, Apr 22, 2024, 2:31 PM -07:00

--- js/hangman.js
+++ js/hangman.js
@@ -1,1 +1,11 @@
-const wordEl = document
+const wordEl = document.getElementById('word')
+const wrongLettersEl = document.getElementById('wrong-letters')
+const playAgainBtn = document.getElementById('play-again')
+const popup = document.getElementById('popup-container')
+const notification = document.getElementById('notification-container')
+const finalMessage = document.getElementById('final-message')
+const figureParts = document.querySelectorAll('.figure-part')
+
+const word = ['application', 'programming', 'interface', 'wizard']
+
+
