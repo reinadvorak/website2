1be177095ab05e6ff90a012384c239b8a50791e6
reinadvorak <142573724+reinadvorak@users.noreply.github.com>
2024-04-29 00:27:51 +0000

    Sun, Apr 28, 2024, 5:27 PM -07:00

--- js/hangman.js
+++ js/hangman.js
@@ -1,6 +1,6 @@
 const wordEl = document.getElementById('word')
 const wrongLettersEl = document.getElementById('wrong-letters')
-const playAgainBtn = document.getElementById('play-again')
+const playAgainBtn = document.getElementById('play-button')
 const popup = document.getElementById('popup-container')
 const notification = document.getElementById('notification-container')
 const finalMessage = document.getElementById('final-message')
@@ -34,6 +34,40 @@ function displayWord() {
     }
 }
 
+//Update the wrong letters
+function updateWrongLettersEl() {
+    //Display wrong letters
+    wrongLettersEl.innerHTML = `
+    ${wrongLetters.length > 0 ? '<p>Wrong</p>' : ''}
+    ${wrongLetters.map(letter => `<span>${letter}</span>`)}
+    `
+    //Display parts
+    figureParts.forEach((part, index) => {
+        const errors = wrongLetters.length
+
+        if(index < errors) {
+            part.style.display = 'block'
+        } else {
+            part.style.display = 'none'
+        }
+    })
+
+    //Check if lost
+    if (wrongLetters.length == figureParts.length) {
+        finalMessage.innerText = 'Unfortunately you lost!'
+        popup.style.display = 'flex'
+    }
+}
+
+// Show Notification
+function showNotification() {
+    notification.classList.add('show')
+
+    setTimeout(() => {
+        notification.classList.remove('show')
+    }, 2000)
+}
+
 //Keydown letter press
 window.addEventListener('keydown', e => {
     if (e.keyCode >= 65 && e.keyCode <= 90) {
@@ -60,5 +94,19 @@ window.addEventListener('keydown', e => {
     }
 })
 
+// Restart game and play again
+playAgainBtn.addEventListener('click', () => {
+    correctLetters.length = 0
+    wrongLetters.length = 0
+
+    selectedIndex = Math.floor(word.length * Math.random())
+    selectedWord = word[selectedIndex]
+
+    displayWord()
+
+    updateWrongLettersEl()
+
+    popup.style.display = 'none'
+})
 
 displayWord()
